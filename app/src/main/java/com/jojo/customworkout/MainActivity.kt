c314aed1cb79412f17fd6b3a8d4648952482a02a
jojoyel <jolalle974@gmail.com>
2023-10-17 11:25:05 +0200

    first commit

---
+++ app/src/main/java/com/jojo/customworkout/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jojo.customworkout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jojo.customworkout.ui.theme.CustomWorkoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomWorkoutTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
